d87c6dc9db997e511563af734f2222736e67f2f5
brad <bxh9261@g.rit.edu>
2021-02-02 19:02:03 -0500

    simple HTTP homework

---
+++ src/imageResponses.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+const gar = fs.readFileSync(`${__dirname}/../client/spongegar.png`);
+
+const getGar = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'image/png' });
+  response.write(gar);
+  response.end();
+};
+
+module.exports.getGar = getGar;
